0e9d222e305712e3939ac908ae935240b53c318b
ramesh <ranilvannan@gmail.com>
2021-07-07 16:23:14 +0530

    First commit

--- models/backlink.py
+++ models/backlink.py
@@ -1,12 +1,12 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 
 class BacklinkGenerator(models.Model):
     _name = "backlink.generator"
     _description = "Backlink Generator"
-    _rec_name = "domain"
 
-    domain = fields.Char(string="Domain", required=True)
+    domain_url = fields.Char(string="Domain", required=True)
     item_ids = fields.One2many(comodel_name="backlink.generator.item", inverse_name="backlink_id")
     comment = fields.Text(string="Comment")
 
@@ -15,7 +15,7 @@ class BacklinkGeneratorItem(models.Model):
     _name = "backlink.generator.item"
     _description = "Backlink Generator Item"
 
-    date = fields.Date(string="Date", required=True)
+    date = fields.Date(string="Date", default=datetime.now(), required=True)
     domain_url = fields.Char(string="Domain URL", required=True)
     backlink_id = fields.Many2one(comodel_name="backlink.generator", string="Backlink", required=True)
     comment = fields.Text(string="Comment")
